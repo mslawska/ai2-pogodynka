7bc223781af81db03366055d2510a2545db795e1
Martyna Sławska <sm53873>
2025-10-31 16:40:29 +0100

    labd

---
+++ src/Form/MeasurementType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Location;
+use App\Entity\Measurement;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MeasurementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('date', DateType::class)
+            ->add('celsius')
+            ->add('wind_speed')
+            ->add('humidity')
+            ->add('pressure')
+            ->add('location', EntityType::class, [
+                'class' => Location::class,
+                'choice_label' => 'city',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Measurement::class,
+        ]);
+    }
+}
